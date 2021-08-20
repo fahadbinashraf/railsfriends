6a76b94c8d0fe01bf22583f6491ede08aebec083
Fahad <mfahadbinashraf@gmail.com>
2021-08-20 13:35:21 +0200

    [TASK] Finalized features
    - added user association with friends
    - updated styles
    - set root to friends index (list)
    - added user logged in checks

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # get 'home/index'
   get 'home/about'
   # for the root home page
-  root 'home#index'
+  root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
